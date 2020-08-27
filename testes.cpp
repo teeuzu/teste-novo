23daa7a43f658138fc73f792df4024b3fc66ee0a
teeuzu <teeuzudo@gmail.com>
2020-08-27 17:59:56 -0300

    importacao legal

---
+++ testes.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main (){
+	float ven,pag,valor,media;
+	int soma,n1,n2,n3;
+	
+	printf("=============================================================");
+	printf("SEJA BEM VINDO A LOJA X, RECEBA DESCONTOS COM COMPRAS ACIMA DE 1.000");
+	printf("\n\n");
+	printf("nota 1"); scanf("%d", &n1);
+	printf("nota 2"); scanf("%d", &n2);
+	printf("nota 3"); scanf("%d", &n3);
+	
+}
